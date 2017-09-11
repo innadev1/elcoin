61a843c96d1720a85205f7635666fd8e77b38e9b
innadev1 <innad2201@gmail.com>
2017-09-11 18:32:42 +0300

    new

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-sm-4 col-sm-offset-4">
-							<div class="my-progress-bar">
+							<div class="my-progress-bar circular-progress-bar">
 								<div class="card"><img src="img/card.png"></div>
 								
 								<div class="text text1">
--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,27 +1,38 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
     $(".my-progress-bar").circularProgress({
         line_width: 2,
         color: "#2aa0c1",
         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
         percent: 0, // percent starts from
         percentage: true,
-    }).circularProgress('animate', 100, 3000);
-});
+    })
+	.circularProgress('animate', 100, 3000);
+});*/
 
-/*$(document).ready(function() {
-    var jqBar = $('.my-progress-bar');
-    var jqBarStatus = true;
+$(document).ready(function() {
+    var jqBar1 = $('.my-progress-bar');
+    var jqBar1Status = true;
+	
+	
+	function circule(){
+		$(".my-progress-bar").circularProgress({
+			line_width: 2,
+			color: "#2aa0c1",
+			starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+			percent: 0, // percent starts from
+			percentage: true,
+		}).circularProgress('animate', 100, 3000);
+	}
+	
+	
+	
     $(window).scroll(function() {
-        var scrollEvent = ($(window).scrollTop() > (jqBar.position().top - $(window).height()));
-        if (scrollEvent && jqBarStatus) { 
-            jqBarStatus = false;
-            $(".my-progress-bar").circularProgress({
-				line_width: 2,
-				color: "#2aa0c1",
-				starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
-				percent: 0, // percent starts from
-				percentage: true,
-			}).circularProgress('animate', 100, 3000);
+        var scrollEvent = ($(window).scrollTop() > (jqBar1.position().top - $(window).height()));
+        if (scrollEvent && jqBar1Status) { 
+			console.log(scrollEvent,jqBar1Status)
+            jqBar1Status = false;
+			circule();
+		
         }
     });
-});*/
+});
