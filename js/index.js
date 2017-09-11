6dbce1c107b3e997ed512cb0e97dcfaf48452aaf
innadev1 <innad2201@gmail.com>
2017-09-11 17:50:18 +0300

    new

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
 	var bar = new ProgressBar.Line(container, {
 	 strokeWidth: 4,
 	 easing: 'easeInOut',
@@ -27,4 +27,44 @@ style: {
 	});
 
 	bar.animate(0.3);  // Number from 0.0 to 1.0
-}); 
+}); */
+
+
+$(function(){
+    var jqBar = $('#container');
+    var jqBarStatus = true;
+    $(window).scroll(function() {
+        var scrollEvent = ($(window).scrollTop() > (jqBar.position().top - $(window).height()));
+        if (scrollEvent && jqBarStatus) { 
+            jqBarStatus = false;
+		var bar = new ProgressBar.Line(container, {
+			 strokeWidth: 4,
+			 easing: 'easeInOut',
+			 duration: 6000,
+			 color: '#007bae',
+			 trailColor: '#54cbdf',
+			 trailWidth: 1,
+			 svgStyle: {width: '100%', height: '50%'},
+			 from: {color: '#007bae'},
+			 to: {color: '#00feff'},
+			text: {
+		style: {
+		 // Text color.
+		 // Default: same as stroke color (options.color)
+			 right: '0',
+			 top: '30px',
+			 padding: 0,
+			 margin: '-5vw',
+			 transform: null
+			},
+			autoStyleContainer: false
+			},
+			step: (state, bar) => {
+			bar.setText(Math.round(bar.value() * 100) + ' %');
+			}
+			});
+
+	bar.animate(0.3);  // Number from 0.0 to 1.0
+        }
+    });
+});
--- js/scripts/script.js
+++ js/scripts/script.js
@@ -5,5 +5,23 @@ $(document).ready(function() {
         starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
         percent: 0, // percent starts from
         percentage: true,
-    }).circularProgress('animate', 100, 5000);
+    }).circularProgress('animate', 100, 3000);
 });
+
+/*$(document).ready(function() {
+    var jqBar = $('.my-progress-bar');
+    var jqBarStatus = true;
+    $(window).scroll(function() {
+        var scrollEvent = ($(window).scrollTop() > (jqBar.position().top - $(window).height()));
+        if (scrollEvent && jqBarStatus) { 
+            jqBarStatus = false;
+            $(".my-progress-bar").circularProgress({
+				line_width: 2,
+				color: "#2aa0c1",
+				starting_position: 0, // 12.00 o' clock position, 25 stands for 3.00 o'clock (clock-wise)
+				percent: 0, // percent starts from
+				percentage: true,
+			}).circularProgress('animate', 100, 3000);
+        }
+    });
+});*/
